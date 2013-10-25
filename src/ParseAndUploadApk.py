aa5265cd3390a7ed389ce02fa31dc329e3464a24
hufeng <hufeng03@gmail.com>
2013-10-25 11:21:33 +0800

    write result to file

--- src/ParseAndUploadApk.py
+++ src/ParseAndUploadApk.py
@@ -8,9 +8,10 @@ import os
 from GetApkDetails import getAllApkDetails
 from OssUploadApk import uploadApk
 
-sep = "============================================"
+sep = "============================================\n"
 
 def ParseAndUploadApk(apkFileOrDir) :
+    fileHandle = open('parse_upload_result.txt','a')
     apkDetailsArray = getAllApkDetails(apkFileOrDir)
     for apkDetails in apkDetailsArray :
         iconArray = apkDetails['iconFileArray']
@@ -34,15 +35,25 @@ def ParseAndUploadApk(apkFileOrDir) :
                 name_array_string += '%s, ' % name
 
         print sep
+        fileHandle.writelines(sep)
         print "应用名称(app_name): [%s]"%name_array_string
+        fileHandle.writelines("应用名称(app_name): [%s]\n"%name_array_string)
         print "安装包名称(package_name): %s"%apkDetails['pkg']
+        fileHandle.writelines("安装包名称(package_name): %s\n"%apkDetails['pkg'])
         print "应用版本号(version_code): %s"%apkDetails['versionCode']
+        fileHandle.writelines("应用版本号(version_code): %s\n"%apkDetails['versionCode'])
         print "应用版本名称(version_name): %s"%apkDetails['versionName']
+        fileHandle.writelines("应用版本名称(version_name): %s\n"%apkDetails['versionName'])
         print "安装包大小(apk_size): %s"%apkDetails['fileSize']
+        fileHandle.writelines("安装包大小(apk_size): %s\n"%apkDetails['fileSize'])
         print "应用图标下载链接(icon_url): %s"%icon_url
+        fileHandle.writelines("应用图标下载链接(icon_url): %s\n"%icon_url)
         print "安装包下载链接(apk_url): %s"%apk_url
+        fileHandle.writelines("安装包下载链接(apk_url): %s\n"%apk_url)
         print sep
+        fileHandle.writelines(sep)
 
+    fileHandle.close()
     jsonResult = json.dumps(apkDetailsArray)
     return jsonResult
 
